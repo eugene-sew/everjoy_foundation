d53571b8bba7d55f5147115fa3b8cc79d58b1026
black-hawk <eugene-sew>
2020-09-25 15:12:16 +0000

    Updates

---
+++ precache-manifest.4512bbed0fd7d0ff2b0c495f79a50371.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9490b9cf1c5f886c352c17f8f9495e8c",
+    "url": "/everjoy_foundation/index.html"
+  },
+  {
+    "revision": "8cd0b645e4f6313bc0b7",
+    "url": "/everjoy_foundation/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "68896444c4e9b9215e38",
+    "url": "/everjoy_foundation/static/js/2.ca9690a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/everjoy_foundation/static/js/2.ca9690a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd0b645e4f6313bc0b7",
+    "url": "/everjoy_foundation/static/js/main.023f326a.chunk.js"
+  },
+  {
+    "revision": "deedfe79df67290e30d0",
+    "url": "/everjoy_foundation/static/js/runtime-main.166655e4.js"
+  },
+  {
+    "revision": "fe3ea178d583913eea0006d787cb1cea",
+    "url": "/everjoy_foundation/static/media/favicon.fe3ea178.svg"
+  },
+  {
+    "revision": "e62422a0a563cc24edf46b82dbdaa6f0",
+    "url": "/everjoy_foundation/static/media/head.e62422a0.jpg"
+  },
+  {
+    "revision": "710a3f96701a31dd0c67b677d26e5004",
+    "url": "/everjoy_foundation/static/media/homecard1i.710a3f96.jpg"
+  },
+  {
+    "revision": "43d8e368fffd1d3d0f08e8ac7d3b0744",
+    "url": "/everjoy_foundation/static/media/homecard2i.43d8e368.jpg"
+  },
+  {
+    "revision": "dcdf9dda3e745cb859694b4901c14615",
+    "url": "/everjoy_foundation/static/media/homecard3i.dcdf9dda.jpg"
+  },
+  {
+    "revision": "685029e3949a8b4361499fcecbebe3fa",
+    "url": "/everjoy_foundation/static/media/slide1.685029e3.jpg"
+  },
+  {
+    "revision": "05d44facecafc402bb0d98f9b2a971db",
+    "url": "/everjoy_foundation/static/media/slide2.05d44fac.jpg"
+  },
+  {
+    "revision": "35ddbbcb24625df8ad1bdd4d6e9e6a96",
+    "url": "/everjoy_foundation/static/media/slide3.35ddbbcb.jpg"
+  }
+]);
